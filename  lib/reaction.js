9dba1458443e730e0a769ead58259a095a25ad76
Paradoxdreamer <esanallen61@gmail.com>
2025-07-16 09:13:39 +0100

    Create reaction.js

---
+++  lib/reaction.js
@@ -0,0 +1,14 @@
+[
+  {
+    "trigger": "marry me",
+    "response": "💀 You couldn't even handle my sarcasm."
+  },
+  {
+    "trigger": "love you",
+    "response": "I'm a codebase with commitment issues."
+  },
+  {
+    "trigger": "paradoxgpt",
+    "response": "what is it you seek mere mortal 🌗"
+  }
+]
